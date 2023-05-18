72af0978b52aae5c8e46a9e5c53fcfe9f8e241c3
nguyennamltv123 <nguyennamltv123@gmail.com>
2023-05-18 15:20:33 +0700

    ver1.2

--- violation_history/urls.py
+++ violation_history/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.get_list_violation, name='violation_list'),
-    path('add_vehicle_violation_API/', views.Add_Violation_History_API.as_view()),
+    # path('add_vehicle_violation_API/', views.Add_Violation_History_API.as_view()),
     path('pay_fee/<int:pk>', views.pay_fee, name='payfee'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout')
